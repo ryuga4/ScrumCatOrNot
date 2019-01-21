517ad9854c5278f6fa2f740529260e5681489b79
ryuga4 <ryuga4@o2.pl>
2019-01-21 02:33:31 +0100

    model change

--- src/Update.elm
+++ src/Update.elm
@@ -9,7 +9,15 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( model + 1, cache (E.bool True) )
+            let
+                newModel =
+                    { model | number = model.number + 1 }
+            in
+            ( newModel, cache (E.bool True) )
 
         Decrement ->
-            ( model - 1, Cmd.none )
+            let
+                newModel =
+                    { model | number = model.number - 1 }
+            in
+            ( newModel, Cmd.none )
